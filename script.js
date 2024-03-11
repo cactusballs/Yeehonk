6a21df03eb3f9b2584c667d246a1a4e0e86165df
cactusballs <71734928+cactusballs@users.noreply.github.com>
2024-03-11 11:59:29 +0000

    animating, indexing

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
+
 jQuery(document).ready(function($) {
   var width = $(document).width();
+  var $idAnimate = $('#animate');
+  var isPaused = false; 
 
   function goRight() {
-    $("#animate").animate({
-      left: width
-    }, 5000, function() {
-      setTimeout(goLeft, 50);
-    });
+    if (!isPaused) {
+      $idAnimate.animate({
+        left: width
+      }, 500, function() {
+        setTimeout(goLeft, 50);
+      });
+    }
   }
 
   function goLeft() {
-    $("#animate").animate({
-      left: 0
-    }, 5000, function() {
-      setTimeout(goRight, 50);
-    });
+    if (!isPaused) {
+      $idAnimate.animate({
+        left: 0
+      }, 500, function() {
+        setTimeout(goRight, 50);
+      });
+    }
   }
 
   setTimeout(goRight, 50);
+
+  // Toggle animation pause/resume on click
+  var $document = $(document);
+  $document.click(function() {
+    if (isPaused) {
+      isPaused = false; // Resume animation
+      goRight(); // Continue animation
+    } else {
+      isPaused = true; // Pause animation
+      $idAnimate.stop(); // Stop the current animation
+    }
+  });
 });
+
